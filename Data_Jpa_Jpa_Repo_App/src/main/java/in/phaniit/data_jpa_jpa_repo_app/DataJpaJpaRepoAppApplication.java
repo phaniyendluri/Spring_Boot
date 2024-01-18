87144f8b520fba6c718772e4d84d323a133eff9d
phani yendluri <yphani0298@gmail.com>
2024-01-18 00:00:31 -0600

    new second commit jpa repo

---
+++ Data_Jpa_Jpa_Repo_App/src/main/java/in/phaniit/data_jpa_jpa_repo_app/DataJpaJpaRepoAppApplication.java
@@ -0,0 +1,13 @@
+package in.phaniit.data_jpa_jpa_repo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaJpaRepoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaJpaRepoAppApplication.class, args);
+	}
+
+}
